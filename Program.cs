d362df1112ecb0dc6371af1bee6ec91d59f71018
gauravkumarit8 <gaurav.kumarit8@gmail.com>
2023-12-29 10:51:22 +0530

    Main file added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace IPLProject{
+    public class IPLProject{
+        static void Main(string[] args){
+            
+        }
+    }
+}
