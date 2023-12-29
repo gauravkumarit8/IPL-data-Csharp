47fa2081668c079e20bdff3790550c2cb637b5f4
gauravkumarit8 <gaurav.kumarit8@gmail.com>
2023-12-29 10:46:31 +0530

    Delivery data added

---
+++ Delivery.cs
@@ -0,0 +1,65 @@
+using System;
+namespace IPLProject{
+    class Delivery{
+        private String MatchId{
+            get;
+            set;
+        }
+        private  String BattingTeam{
+            get;
+            set;
+        }
+        private  String BowlingTeam{
+            get;
+            set;
+        }
+        private Integer Over{
+            get;
+            set;
+        }
+        private  Integer Ball{
+            get;
+            set;
+        }
+        private  String Batsmen{
+            get;
+            set;
+        }
+        private String Bowler{
+            get;
+            set;
+        }
+        private Integer BatsmanRuns{
+            get;
+            set;
+        }
+        private Integer TotalRuns{
+            get;
+            set;
+        }
+        private int LegByeRuns{
+            get;
+            set;
+        }
+        private int ByesRuns{
+            get;
+            set;
+        }
+        private int PenalityRuns{
+            get;
+            set;
+        }
+        private int NoBall{
+            get;
+            set;
+        }
+        private  int Wide{
+            get;
+            set;
+        }
+        private Integer ExtraRuns{
+            get;
+            set;
+        }
+    }
+}
---
+++ Match.cs
@@ -0,0 +1,39 @@
+using System;
+namespace IPLProject{
+    class Match{
+        private String Id
+        {
+            get;
+            set;
+        }
+        private String Year{
+            get;
+            set;
+        }
+        private String Team1{
+            get;
+            set;
+        }
+        private String Team2{
+            get;
+            set;
+        }
+        private String City{
+            get;
+            set;
+        }
+        private String WinnerTeam{
+            get;
+            set;
+        }
+        private String PlayerOfMatch{
+            get;
+            set;
+        }
+        private String Venue{
+            get;
+            set;
+        }
+
+    }
+}
